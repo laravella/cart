db22d0f4d356c6dc99768258facd3cef47fe87c4
laravella <laravella.cms@gmail.com>
2013-08-07 16:51:24 +0200

    product seeder

--- src/database/seeds/ProductsSeeder.php
+++ src/database/seeds/ProductsSeeder.php
@@ -1,12 +1,19 @@
 <?php class ProductsSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
+        private function __addProduct($name, $model, $size) {
+            $product = array('name'=>$name, 'model'=>$model, 'size'=>$size);     
+            $productId = DB::table('products')->insertGetId($product);
+//            Log::write('info', $label.' menu created');
+            return $productId;
+        }
+        
 	public function run()
 	{
+
+		DB::table('products')->delete();
+                
+                //$this->__addProduct('name', 'model', 'size');
         }
+        
 
 }
